2c50f6b5d7bc995875b00b4e12b9d228cf16f2a7
Daniel Grant <daniel.grant@digirati.com>
2018-03-22 10:19:07 +0000

    [RDSS_1973] Added DynamoDB tests.

--- tests/test_dynamodb_client.py
+++ tests/test_dynamodb_client.py
@@ -5,19 +5,16 @@ from dateutil import parser
 from moto import mock_dynamodb2
 from app import DynamoDBClient
 
-test_high_watermark_last_updated = parser.parse('2018-03-20T08:48:04')
-test_high_watermark_value = parser.parse('2018-03-20T00:00:09')
-
 
 @mock_dynamodb2
-def test_fetch_high_watermark():
+def test_watermark_table():
     # Create the DynamoDB client we'll be testing again
     dynamodb_client = DynamoDBClient(
         'rdss-eprints-adaptor-watermark-test',
         'rdss-eprints-adaptor-processed-test'
     )
 
-    # Create a Boto3 DynamoDB client we'll use to create the mock table and populate it
+    # Create a Boto3 DynamoDB client we'll use to create the mock table
     boto3_client = boto3.client('dynamodb')
     boto3_client.create_table(
         TableName='rdss-eprints-adaptor-watermark-test',
@@ -48,9 +45,63 @@ def test_fetch_high_watermark():
     assert high_watermark is None
 
     # Populate a high watermark row into the DynamoDB table
+    test_high_watermark_value = parser.parse('2018-03-20T00:00:09')
     dynamodb_client.update_high_watermark(test_high_watermark_value)
 
     # Verify that we get the correct response, with a second appended to the given high watermark
     high_watermark = dynamodb_client.fetch_high_watermark()
-    assert high_watermark is not None
     assert high_watermark == test_high_watermark_value + timedelta(seconds=1)
+
+
+@mock_dynamodb2
+def test_processed_table():
+    # Create the DynamoDB client we'll be testing again
+    dynamodb_client = DynamoDBClient(
+        'rdss-eprints-adaptor-watermark-test',
+        'rdss-eprints-adaptor-processed-test'
+    )
+
+    # Create a Boto3 DynamoDB client we'll use to create the mock table
+    boto3_client = boto3.client('dynamodb')
+    boto3_client.create_table(
+        TableName='rdss-eprints-adaptor-processed-test',
+        KeySchema=[
+            {
+                'AttributeName': 'Identifier',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'LastUpdated',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Message',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Reason',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Status',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 20,
+            'WriteCapacityUnits': 60
+        }
+    )
+
+    # Verify that a query that returns no rows gives us a 'None' response
+    processed_status = dynamodb_client.fetch_processed_status('eprints-identifier-test')
+    assert processed_status is None
+
+    # Populate a processed record into the DynamoDB table
+    dynamodb_client.update_processed_record('eprints-identifier-test', '{}', 'Success', '-')
+
+    # Verify that we get the correct response
+    processed_status = dynamodb_client.fetch_processed_status('eprints-identifier-test')
+    assert processed_status == 'Success'
