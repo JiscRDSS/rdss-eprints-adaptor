4cc67599da247a670cc6a88244494c37f38e03dc
Finlay McCourt <finlay.mccourt@digirati.com>
2018-09-24 11:06:32 +0100

    Adding in changes from previous hotfix branch.

--- app/message_generator.py
+++ app/message_generator.py
@@ -88,6 +88,13 @@ class MessageGenerator(object):
             logging.warning('DC metadata [\'%s\'] has more than 1 value', key)
         return values[0]
 
+    def _unique_value_list_from_dc_metadata(self, dc_metadata, key):
+        values = dc_metadata.get(key)
+        if not values:
+            logging.warning('DC metadata [%s] does not contain [\'%s\'] field', dc_metadata, key)
+            return []
+        return list(set(values))
+
     def _extract_object_title(self, dc_metadata):
         return self._single_value_from_dc_metadata(dc_metadata, 'title')
 
@@ -104,9 +111,11 @@ class MessageGenerator(object):
         return rights_statement
 
     def _extract_object_date(self, dc_metadata):
-        return parser.parse(
-            self._single_value_from_dc_metadata(dc_metadata, 'date')
-        ).isoformat()
+        date_string = self._single_value_from_dc_metadata(dc_metadata, 'date')
+        if not date_string:
+            return None
+        else:
+            return parser.parse(date_string).isoformat()
 
     def _extract_object_person_roles(self, dc_metadata):
         def _object_person_role(name, role_enum):
@@ -125,13 +134,13 @@ class MessageGenerator(object):
                 'role': role_enum
             }
         people = dc_metadata.get('creator', []) + dc_metadata.get('contributor', [])
-        return [_object_person_role(person, 21) for person in people]
+        return [_object_person_role(person, 21) for person in set(people)]
 
     def _extract_object_keywords(self, dc_metadata):
-        return dc_metadata.get('subject', [])
+        return self._unique_value_list_from_dc_metadata(dc_metadata, 'subject')
 
     def _extract_object_category(self, dc_metadata):
-        return dc_metadata.get('subject', [])
+        return self._unique_value_list_from_dc_metadata(dc_metadata, 'subject')
 
     def _doi_identifier(self, value):
         return {
@@ -140,13 +149,14 @@ class MessageGenerator(object):
         }
 
     def _extract_object_identifier_value(self, dc_metadata):
-        return [self._doi_identifier(_id) for _id in dc_metadata.get('identifier', [])]
+        return [self._doi_identifier(_id) for _id in
+                self._unique_value_list_from_dc_metadata(dc_metadata, 'identifier')]
 
     def _extract_object_related_identifier(self, dc_metadata):
         return [{
             'identifier': self._doi_identifier(rel),
             'relationType': 13
-        } for rel in dc_metadata.get('relation', [])]
+        } for rel in self._unique_value_list_from_dc_metadata(dc_metadata, 'relation')]
 
     def _extract_object_organisation_role(self, dc_metadata):
         return [{
@@ -155,7 +165,8 @@ class MessageGenerator(object):
                     'organisationName': publisher
                 },
                 'role': 5
-                } for publisher in dc_metadata.get('publisher', [])]
+                } for publisher in
+                self._unique_value_list_from_dc_metadata(dc_metadata, 'publisher')]
 
     def _extract_object_files(self, s3_objects):
         return [{
